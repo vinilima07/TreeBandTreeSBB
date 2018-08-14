a59840b2a8918d3d6a37d7bcbd6442257c247d9c
Vinicius França <vinilima07@gmail.com>
2018-08-14 09:00:58 -0300

    Upando praticas de aeds

---
+++ AEDS_Pratica1/src/aed/Item.java
@@ -0,0 +1,28 @@
+package aed;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *Elemento (dado) que constituira a arvore
+ */
+public class Item {
+    private int chave;
+    public Item(int chave) {
+        this.chave = chave;
+    }
+    public int compara(Item it) {
+        Item item = it;
+        if (this.chave < item.chave)
+            return -1;
+        else if (this.chave > item.chave)
+            return 1;
+        return 0;
+    }
+    public int getChave() {
+        return chave;
+    }
+}
